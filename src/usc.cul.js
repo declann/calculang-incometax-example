11ff6ce429b4e2c6ae1ddfacac40294965a1ed8d
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-17 10:33:21 +0000

    cleanup usc {}

--- src/usc.cul.js
+++ src/usc.cul.js
@@ -17,25 +17,25 @@ export const usc_co_3_ytd = () => {
 export const usc_liab_ytd = () => {
   return (
     (annual_salary() >= 10036) *
-      (Math.min(gross_salary_ytd({}), usc_co_1_ytd({})) *
+      (Math.min(gross_salary_ytd(), usc_co_1_ytd()) *
         (t().getYear() == 114 ? 0.02 : 0.015) +
         Math.max(
           0,
           Math.min(
-            usc_co_2_ytd({}) - usc_co_1_ytd({}),
-            gross_salary_ytd({}) - usc_co_1_ytd({})
+            usc_co_2_ytd() - usc_co_1_ytd(),
+            gross_salary_ytd() - usc_co_1_ytd()
           )
         ) *
           (t().getYear() == 114 ? 0.04 : 0.035) +
-        Math.max(0, gross_salary_ytd({}) - usc_co_2_ytd({})) * 0.07) +
+        Math.max(0, gross_salary_ytd() - usc_co_2_ytd()) * 0.07) +
     (t().getYear() != 114) *
-      Math.max(0, gross_salary_ytd({}) - usc_co_3_ytd({})) *
+      Math.max(0, gross_salary_ytd() - usc_co_3_ytd()) *
       0.01
   ); // 8% above CO 3 2015
   // TODO 2015 self-assessed rate of 11% above 100k (from 10% 2014)
 };
 
 export const usc_liab = () => {
-  if (t().getDate() == 1 && t().getMonth() == 0) return usc_liab_ytd({});
-  return usc_liab_ytd({}) - usc_liab_ytd({ t_in: addMonths(t(), -1) });
+  if (t().getDate() == 1 && t().getMonth() == 0) return usc_liab_ytd();
+  return usc_liab_ytd() - usc_liab_ytd({ t_in: addMonths(t(), -1) });
 };
